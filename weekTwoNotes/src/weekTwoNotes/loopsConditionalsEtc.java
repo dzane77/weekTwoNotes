f9aed4fdbf0793affa8457ef97c79d63bad432c8
dzane77 <dedrazane@gmail.com>
2022-11-16 10:40:35 -0700

    first commit

---
+++ weekTwoNotes/src/weekTwoNotes/loopsConditionalsEtc.java
@@ -0,0 +1,13 @@
+package weekTwoNotes;
+
+public class loopsConditionalsEtc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//declaring a for loop
+	//	for (int i = 0; i < 10; i++) {
+		//	System.out.println("scoop cup of flour");
+		}
+	}
+
+}
